2bce32acd1fcc0668f674919eb4ad634582f59a8
rainingmaster <312841925@qq.com>
2020-04-26 11:01:52 +0800

    feature: keepalive for ngx balancer

--- lib/ngx/balancer.lua
+++ lib/ngx/balancer.lua
@@ -8,6 +8,7 @@ base.allows_subsystem('http', 'stream')
 local ffi = require "ffi"
 local C = ffi.C
 local ffi_str = ffi.string
+local ffi_new = ffi.new
 local errmsg = base.get_errmsg_ptr()
 local FFI_OK = base.FFI_OK
 local FFI_ERROR = base.FFI_ERROR
@@ -18,6 +19,7 @@ local type = type
 local tonumber = tonumber
 local max = math.max
 local subsystem = ngx.config.subsystem
+local pargs
 local ngx_lua_ffi_balancer_set_current_peer
 local ngx_lua_ffi_balancer_set_more_tries
 local ngx_lua_ffi_balancer_get_last_failure
@@ -26,8 +28,17 @@ local ngx_lua_ffi_balancer_set_timeouts -- used by both stream and http
 
 if subsystem == 'http' then
     ffi.cdef[[
+    typedef struct {
+        const unsigned char  *name_data;
+        size_t                name_len;
+        uintptr_t             max_cached;
+        uintptr_t             requests;
+        uintptr_t             timeout;
+    } ngx_http_lua_ffi_keepalive_args_t;
+
     int ngx_http_lua_ffi_balancer_set_current_peer(ngx_http_request_t *r,
-        const unsigned char *addr, size_t addr_len, int port, char **err);
+        const unsigned char *addr, size_t addr_len, int port,
+        ngx_http_lua_ffi_keepalive_args_t *args, char **err);
 
     int ngx_http_lua_ffi_balancer_set_more_tries(ngx_http_request_t *r,
         int count, char **err);
@@ -39,6 +50,8 @@ if subsystem == 'http' then
         long connect_timeout, long send_timeout,
         long read_timeout, char **err);
     ]]
+
+    pargs = ffi_new("ngx_http_lua_ffi_keepalive_args_t [1]")
 
     ngx_lua_ffi_balancer_set_current_peer =
         C.ngx_http_lua_ffi_balancer_set_current_peer
@@ -103,7 +116,47 @@ local peer_state_names = {
 local _M = { version = base.version }
 
 
-function _M.set_current_peer(addr, port)
+local function check_keepalive_args(opt)
+    if not opt then
+        return
+    end
+
+    if type(opt) ~= "table" then
+        error("bad opt arg: table expected, got " .. type(opt), 2)
+    end
+
+    local pool = opt.pool
+    local max_cached = opt.max_cached
+    local requests = opt.requests
+    local timeout = opt.timeout
+
+    if type(pool) ~= "string" then
+        error("bad pool arg: string expected, got " .. type(pool), 2)
+    end
+
+    if type(opt.max_cached) ~= "number" then
+        error("bad max_cached", 2)
+    end
+
+    if type(opt.requests) ~= "number" or opt.requests <= 0 then
+        requests = 100
+    end
+
+    if type(opt.timeout) ~= "number" or opt.timeout <= 0 then
+        timeout = 60000
+    end
+
+    pargs[0].name_data = pool
+    pargs[0].name_len = #(pool)
+    pargs[0].max_cached = max_cached
+    pargs[0].requests = requests
+    pargs[0].timeout = timeout
+
+    return pargs
+end
+
+
+function _M.set_current_peer(addr, port, opt)
     local r = get_request()
     if not r then
         error("no request found")
@@ -115,8 +168,17 @@ function _M.set_current_peer(addr, port)
         port = tonumber(port)
     end
 
-    local rc = ngx_lua_ffi_balancer_set_current_peer(r, addr, #addr,
-                                                     port, errmsg)
+    local rc
+
+    if subsystem == 'http' then
+        local p = check_keepalive_args(opt)
+        rc = C.ngx_http_lua_ffi_balancer_set_current_peer(r, addr, #addr,
+                                                          port, p, errmsg)
+    else
+        rc = C.ngx_stream_lua_ffi_balancer_set_current_peer(r, addr, #addr, port,
+                                                            errmsg)
+    end
+
     if rc == FFI_OK then
         return true
     end
